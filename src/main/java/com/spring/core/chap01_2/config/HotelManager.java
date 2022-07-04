06aad81bab9f81260b6d2218687b2ffe79caab81
hwseo0705 <julieseo0705@gmail.com>
2022-07-04 15:57:07 +0900

    [07.04.22] Spring Commit

--- src/main/java/com/spring/core/chap01_2/config/HotelManager.java
+++ src/main/java/com/spring/core/chap01_2/config/HotelManager.java
@@ -4,7 +4,9 @@ import com.spring.core.chap01_2.*;
 
 // Only the HotelManager has to change the Hotel Restaurant info
 // None of the other classes have to change
+
 // 호텔에 필요한 객체들을 생성하고 조립하여 의존관계를 설정해주는 역할
+// Bean Factory : Spring Container
 public class HotelManager {
 
     // 쉐프 객체를 생성하여 리턴하는 메서드
